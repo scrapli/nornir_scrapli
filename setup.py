dd6fdb62750c725d99160c22e4dcac5221db89d2
Carl Montanari <carl.r.montanari@gmail.com>
2020-03-22 09:12:14 -0700

    initial structure copied from davids napalm proposal

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""nornir_scrapli - scrapli nornir plugin"""
+import setuptools
+
+__author__ = "Carl Montanari"
+
+with open("README.md", "r") as f:
+    README = f.read()
+
+setuptools.setup(
+    name="nornir_scrapli",
+    version="2020.03.21",
+    author=__author__,
+    author_email="carl.r.montanari@gmail.com",
+    description="scrapli Nornir plugin",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/carlmontanari/nornir_scrapli",
+    packages=setuptools.find_packages(),
+    install_requires=["scrapli>=2020.03.21"],
+    extras_require={},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+    ],
+    python_requires=">=3.6",
+)
