457c5cda9ee0699e10bcb684175f8a6f75f56ba9
Carl Montanari <carl.r.montanari@gmail.com>
2024-07-28 09:26:28 -0700

    chore: add --pre flag for (python) pre-release build

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,6 @@
 """nornir_scrapli.noxfile"""
 
+import os
 import re
 import sys
 from pathlib import Path
@@ -10,6 +11,8 @@ import nox
 nox.options.error_on_missing_interpreters = False
 nox.options.stop_on_first_error = False
 nox.options.default_venv_backend = "venv"
+
+PRE = bool(os.environ.get("PRE_RELEASE"))
 
 
 def parse_requirements(dev: bool = True) -> Dict[str, str]:
@@ -64,6 +67,18 @@ PLATFORM: str = sys.platform
 SKIP_LIST: List[str] = []
 
 
+def _get_install_test_args() -> List[str]:
+    args = [".[dev]"]
+
+    if sys.platform == "darwin":
+        args = [".[dev-darwin]"]
+
+    if PRE:
+        args.append("--pre")
+
+    return args
+
+
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
 def unit_tests(session):
     """
@@ -83,7 +98,7 @@ def unit_tests(session):
         return
 
     session.install("-U", "setuptools", "wheel", "pip")
-    session.install(".[dev]")
+    session.install(*_get_install_test_args())
     session.run(
         "python",
         "-m",
@@ -153,7 +168,7 @@ def pylama(session):
         N/A
 
     """
-    session.install(".[dev]")
+    session.install(*_get_install_test_args())
     session.run("python", "-m", "pylama", ".")
 
 
